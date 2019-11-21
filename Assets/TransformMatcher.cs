6ba91134b91b7bb71915820ecf9c705c1c883264
shurm <michaelshur28@gmail.com>
2019-11-21 14:21:54 -0500

    better lighting for corridor

---
+++ Assets/TransformMatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Wrapper
+{
+    public Transform currentPos;
+    public Transform desiredPos;
+}
+
+public class TransformMatcher : MonoBehaviour
+{
+    
+    public Wrapper[] transformTable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach( Wrapper w in transformTable)
+        {
+            Transform currentPos = w.currentPos;
+            Transform desiredPos = w.desiredPos;
+
+            currentPos.position = desiredPos.position;
+        }
+    }
+}
