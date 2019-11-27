b86c9ef6f0a781db17ad246d524720bdddb3a180
Prutha <prutha@seas.upenn.edu>
2019-11-26 19:35:28 -0500

    Slicing

---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class Slice : MonoBehaviour
+{
+    public Material capMaterial;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+        {
+            pieces[1].AddComponent<Rigidbody>();
+        }
+
+        Destroy(pieces[1], 1);
+    }
+}
