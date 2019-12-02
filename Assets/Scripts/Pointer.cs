5bd95dd2831f4908fef02ee1787d1d2f9f058c16
chloele33 <chloele@seas.upenn.edu>
2019-12-02 11:14:19 -0500

    start pointer

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float m_DefaultLength = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRenderer = null;
+
+    private void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        // use default or distance
+        float targetLength = m_DefaultLength;
+
+        // raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        // default
+        Vector3 endPos = transform.position + (transform.forward * targetLength);
+
+        // or based on hit
+
+        if (hit.collider != null)
+            endPos = hit.point;
+
+        // set position of the dot
+        m_Dot.transform.position = endPos;
+
+        // set positin of line renderer
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, endPos);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLength);
+        return hit;
+    }
+}
